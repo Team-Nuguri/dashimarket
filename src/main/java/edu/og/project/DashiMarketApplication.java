bd2eb001bbe8c1353f9c7a3a376fbc0917014401
kyusik <kyusik2910@gmail.com>
2025-09-24 10:44:13 +0900

    Feat. 팀 프로젝트 생성

---
+++ src/main/java/edu/og/project/DashiMarketApplication.java
@@ -0,0 +1,13 @@
+package edu.og.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashiMarketApplication.class, args);
+	}
+
+}
