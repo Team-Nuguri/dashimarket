07ca6bd68b49aaf84517f786c500efe330352ada
yesol <gkdpthf1@naver.com>
2025-09-25 22:03:22 +0900

    Feat. 인트로, 굿즈 홈페이지, 커뮤니티 홈페이지 추가 Rename. static 및 templates 폴더 생성, 파일 세분화

---
+++ DashiMarket/src/main/resources/static/js/goodsPage/goodsHome.js
@@ -0,0 +1,33 @@
+console.log("goosHome.js");
+
+const sortText = document.getElementById("sort-text");
+const sortBox = document.getElementById("select-sort-box");
+const selectSort = document.getElementById("select-sort");
+const dropDownIcon = document.querySelector("#drop-down-icon img");
+
+/* 정렬 선택창 토글 */
+selectSort.addEventListener("click", () => {
+    /* toggle의 리턴값: true = 열림, false = 닫힘 */
+    const isOpen = sortBox.classList.toggle("show");
+
+    if (isOpen) { // 드롭다운 펼쳐졌을 때
+        dropDownIcon.setAttribute("src", "/images/svg/drop-down-reverse.svg");
+    } else {
+        dropDownIcon.setAttribute("src", "/images/svg/drop-down.svg");
+    }
+});
+
+/* 정렬 선택시 반영시키기 */
+const sortLinks = document.querySelectorAll("#select-sort-box a");
+
+sortLinks.forEach(link => {
+    link.addEventListener("click", (e) => {
+        /* 이거 나중에 풀어야 함! (쿠키 또는 세션에 유지) */
+        e.preventDefault();
+
+        /* 선택한 정렬 반영하기 */
+        sortText.innerText = link.innerText;
+        sortBox.classList.remove("show");
+        dropDownIcon.setAttribute("src", "/images/svg/drop-down.svg");
+    })
+})
