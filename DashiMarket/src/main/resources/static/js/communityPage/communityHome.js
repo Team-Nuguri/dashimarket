670615aee7f6603b370405d92c7ba99c870fdebb
peachpi3 <gkdpthf1@naver.com>
2025-09-28 00:26:15 +0900

    Feat. 커뮤니티 홈페이지, 관심있는 게시글 페이지 추가 Update. 푸터 수정

--- DashiMarket/src/main/resources/static/js/communityPage/communityHome.js
+++ DashiMarket/src/main/resources/static/js/communityPage/communityHome.js
@@ -1,1 +1,69 @@
 console.log("communityHome.js");
+
+const selectCity = document.getElementById("select-city");
+const cityBox = document.getElementById("select-city-box");
+const cityName = document.getElementById("city-name");
+const dropDownIcon = document.querySelector("#drop-down-icon img");
+
+/* 동네 선택창 토글 */
+selectCity.addEventListener("click", () => {
+    /* toggle의 리턴값: true = 열림, false = 닫힘 */
+    const isOpen = cityBox.classList.toggle("show");
+
+    if (isOpen) { // 드롭다운 펼쳐졌을 때
+        dropDownIcon.setAttribute("src", "/images/svg/drop-down-reverse.svg");
+    } else {
+        dropDownIcon.setAttribute("src", "/images/svg/drop-down.svg");
+    }
+});
+
+/* 동네 선택시 반영시키기 */
+const cityLinks = document.querySelectorAll("#select-city-box a");
+
+cityLinks.forEach(link => {
+    link.addEventListener("click", (e) => {
+        /* 이거 나중에 풀어야 함! (쿠키 또는 세션에 유지) */
+        e.preventDefault();
+
+        /* 선택한 동네 이름으로 반영하기 */
+        cityName.innerText = link.innerText;
+        cityBox.classList.remove("show");
+        dropDownIcon.setAttribute("src", "/images/svg/drop-down.svg");
+    })
+})
+
+/* 카테고리 링크 요소 모두 가져오기 */
+const categoryLinks = document.querySelectorAll("#category-drop-down-container a");
+let isActive = null; // 현재 활성화된 카테고리 링크를 저장할 변수
+
+categoryLinks.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        /* 선택시 선택 효과 */
+        if (isActive) {
+            isActive.classList.remove("active-category"); 
+        }
+
+        link.classList.add("active-category");
+        isActive = link;
+
+    })
+})
+
+/* 정렬 */
+const sort = document.querySelectorAll("#sort-area a");
+let isSort = null;
+
+sort.forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if(isSort) {
+      isSort.classList.remove("bold-text");
+    }
+
+    link.classList.add("bold-text");
+    isSort = link;
+  })
+})
