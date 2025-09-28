5e8609f768d43ec119b93f390c4a2ae6aaa5e077
kyusik <kyusik2910@gmail.com>
2025-09-28 20:20:58 +0900

    Feat. 주문페이지 html css js 추가

---
+++ DashiMarket/src/main/resources/static/js/order/order.js
@@ -0,0 +1,139 @@
+/* 
+    추가로 해야할 것
+    결제 요청 시 
+    
+
+*/
+
+
+
+const orderItem = document.querySelectorAll('.order-item-row');
+
+orderItem.forEach(item => {
+
+    /* 필요한 요소들 가져옴 */
+    const minusBtn = item.querySelector(".minus");
+    const plusBtn = item.querySelector(".plus");
+    const quantityInput = item.querySelector('.quantity');
+    const totalPrice = item.querySelector(".total-price");
+    const inputPrice = item.querySelector('[name="inputPrice"]');
+    const deleteBtn = item.querySelector(".item-delete>button");
+
+
+    // 상품 개별 가격 숫자로 파싱
+    const unitPriceText = totalPrice.innerText.replace(/[^0-9]/g, '');
+    const unitPrice = parseInt(unitPriceText);
+
+
+    minusBtn.addEventListener("click", e => {
+
+        // 현재 수량 가져옴
+        let quantity = parseInt(quantityInput.value);
+
+        if(quantity > 1){
+            quantity--;
+            quantityInput.value = quantity;
+
+            // 총 가격 계산
+            const realTotal = quantity * unitPrice;
+
+            // 총 가격을 원화 형식으로 바꿔주는 함수 호출해 겨로가 반환 받고 
+            totalPrice.innerText = formatPrice(realTotal);
+            inputPrice.value = realTotal;
+
+            // 총 결제 금액 업데이트
+            updateTotalPrice();
+
+        }
+    })
+
+    plusBtn.addEventListener("click", () => {
+
+        
+        let quantity = parseInt(quantityInput.value);
+        if(quantity > 9){
+            alert("최대 주문 수량은 10개 입니다.");
+            return;
+        }
+
+        if(quantity < 10){
+            quantity++;
+            quantityInput.value = quantity;
+
+            const realTotal = quantity * unitPrice;
+
+            totalPrice.innerText = formatPrice(realTotal);
+            inputPrice.value = realTotal;
+
+            updateTotalPrice();
+        }
+
+    })
+
+
+    /* 삭제 버튼 클릭 시 해당 영역 지우기 */
+    /* 새로고침 시 다시 생김 해결할 수 있음 하기 */
+    deleteBtn.addEventListener("click", e => {
+
+        if(!confirm("해당 상품을 주문 목록에서 삭제하시겠습니까 ?")){
+            alert('삭제 취소');
+            return ;
+        }
+
+        item.remove();
+        updateTotalPrice();
+        alert('삭제되었습니다.');
+
+    })
+
+
+
+})
+
+
+/* 한국 원화 형식으로 바꿔주는 함수 */
+function formatPrice(number) {
+    return number.toLocaleString('ko-KR') + ' 원';
+}
+
+
+/* 총 결제 금액 계산 */
+function updateTotalPrice(){
+
+    let finalTotal = 0;
+
+    document.querySelectorAll('.order-item-row').forEach(item => {
+        const inputPrice = item.querySelector('[name="inputPrice"]'); 
+        const totalPrice = item.querySelector('.total-price');
+
+        // 처음 문서가 로드될 때 수량에 맞는 가격 반영해서 보여주기
+        totalPrice.innerText = formatPrice(parseInt(inputPrice.value));
+
+        finalTotal += parseInt(inputPrice.value)
+
+    });
+
+    document.getElementById("realTotalPrice").value = finalTotal;
+    // 총 결제금액 들어갈 요소 가져와서 원화 형식으로 변경후 innertext
+    const realTotalPriceElement = document.getElementById("real-total-price");
+    if (realTotalPriceElement) {
+        realTotalPriceElement.innerText = formatPrice(finalTotal);
+    }
+
+
+
+}
+
+
+/* 문서 로드 되었을 때 수량하고 계싼해서 처음에 화면 보여주기 */
+document.addEventListener("DOMContentLoaded", () => {
+
+    const orderItems = document.querySelectorAll('.order-item-row');
+
+    updateTotalPrice();
+
+
+});
+
+
+/* 삭제 버튼 클릭 시 요소제거 ? */
