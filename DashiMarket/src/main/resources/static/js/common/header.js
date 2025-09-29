e92764b5e7da3ebe547148717aa32ac1213fca90
Jihyeon <every3322@gmail.com>
2025-09-29 22:16:03 +0900

    Add. 헤더 알림 js 추가

--- DashiMarket/src/main/resources/static/js/common/header.js
+++ DashiMarket/src/main/resources/static/js/common/header.js
@@ -1,2 +1,18 @@
 console.log("header.js loaded..")
 
+// 알림 버튼 클릭시 알림 리스트 보이게 하기
+const notificationBtn = document.getElementById("bell");
+
+notificationBtn.addEventListener("click", ()=>{
+
+    // 알림 목록
+    const notificationList = document.querySelector(".notification-list");
+
+    // 화면에 알림 목록이 보일 때
+    if(notificationList.classList.contains("show")){
+        notificationList.classList.toggle("show")
+
+    }else{
+        notificationList.classList.toggle("show")
+    }
+})
