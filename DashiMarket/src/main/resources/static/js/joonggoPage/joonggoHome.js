4592b19ff9379fb900f708aa9ff868b0127f9526
DESKTOP-8FSFFJK\TJ <eurostring@gmail.com>
2025-10-02 17:35:29 +0900

    Update. 중고 페이지 목록 수정

--- DashiMarket/src/main/resources/static/js/joonggoPage/joonggoHome.js
+++ DashiMarket/src/main/resources/static/js/joonggoPage/joonggoHome.js
@@ -68,3 +68,180 @@ sort.forEach(link => {
     isSort = link;
     })
 })
+
+
+
+document.getElementById("div1").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer1 = document.getElementById("category-sub-container" + i);
+            subContainer1.style.display = (i === 1) ? "block" : "none";
+        }
+  });
+
+  
+document.getElementById("div2").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer2 = document.getElementById("category-sub-container" + i);
+            subContainer2.style.display = (i === 2) ? "block" : "none";
+        }
+  });
+
+
+   
+document.getElementById("div3").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer3 = document.getElementById("category-sub-container" + i);
+            subContainer3.style.display = (i === 3) ? "block" : "none";
+        }
+  });
+
+  
+document.getElementById("div4").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer4 = document.getElementById("category-sub-container" + i);
+            subContainer4.style.display = (i === 4) ? "block" : "none";
+        }
+  });
+
+   
+document.getElementById("div5").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer5 = document.getElementById("category-sub-container" + i);
+            subContainer5.style.display = (i === 5) ? "block" : "none";
+        }
+  });
+
+  
+document.getElementById("div6").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer6 = document.getElementById("category-sub-container" + i);
+            subContainer6.style.display = (i === 6) ? "block" : "none";
+        }
+  });
+
+  
+  
+document.getElementById("div7").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer7 = document.getElementById("category-sub-container" + i);
+            subContainer7.style.display = (i === 7) ? "block" : "none";
+        }
+  });
+
+  
+document.getElementById("div8").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer8 = document.getElementById("category-sub-container" + i);
+            subContainer8.style.display = (i === 8) ? "block" : "none";
+        }
+  });
+
+
+  
+document.getElementById("div9").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer9 = document.getElementById("category-sub-container" + i);
+            subContainer9.style.display = (i === 9) ? "block" : "none";
+        }
+  });
+
+  
+  
+document.getElementById("div10").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer10 = document.getElementById("category-sub-container" + i);
+            subContainer10.style.display = (i === 10) ? "block" : "none";
+        }
+  });
+
+
+  
+document.getElementById("div11").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer11 = document.getElementById("category-sub-container" + i);
+            subContainer11.style.display = (i === 11) ? "block" : "none";
+        }
+  });
+
+
+  
+document.getElementById("div12").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer12 = document.getElementById("category-sub-container" + i);
+            subContainer12.style.display = (i === 12) ? "block" : "none";
+        }
+  });
+
+
+  
+document.getElementById("div13").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer13 = document.getElementById("category-sub-container" + i);
+            subContainer13.style.display = (i === 13) ? "block" : "none";
+        }
+  });
+
+
+  
+document.getElementById("div14").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer14 = document.getElementById("category-sub-container" + i);
+            subContainer14.style.display = (i === 14) ? "block" : "none";
+        }
+  });
+
+
+  
+document.getElementById("div15").addEventListener("click", function() {
+
+        document.getElementById("category-container").style.display = "none";
+
+        for (let i = 1; i <= 15; i++) {
+            const subContainer15 = document.getElementById("category-sub-container" + i);
+            subContainer15.style.display = (i === 15) ? "block" : "none";
+        }
+  });
+
+
