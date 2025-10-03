1b45b3957edca19e0dfff6b547ca7637e1c7d469
peachpi3 <gkdpthf1@naver.com>
2025-10-03 23:30:29 +0900

    굿즈 목록 조회 중

--- DashiMarket/src/main/java/edu/og/project/goods/model/dao/GoodsMapper.java
+++ DashiMarket/src/main/java/edu/og/project/goods/model/dao/GoodsMapper.java
@@ -3,6 +3,8 @@ package edu.og.project.goods.model.dao;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class GoodsMapper {
+public interface GoodsMapper {
+
+	public int getListCount(String boardType);
 
 }
--- DashiMarket/src/main/java/edu/og/project/goods/model/service/GoodsServiceImpl.java
+++ DashiMarket/src/main/java/edu/og/project/goods/model/service/GoodsServiceImpl.java
@@ -2,15 +2,22 @@ package edu.og.project.goods.model.service;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import edu.og.project.goods.model.dao.GoodsMapper;
 
 @Service
 public class GoodsServiceImpl implements GoodsService {
+	
+	@Autowired
+	private GoodsMapper mapper;
 
 	// 굿즈 상품 목록 조회
 	@Override
 	public Map<String, Object> selectGoodsList(String boardType, int cp) {
-		
+		// 삭제되지 않은 게시글 수 조회
+		int listCount = mapper.getListCount(boardType);
 		return null;
 	}
 
