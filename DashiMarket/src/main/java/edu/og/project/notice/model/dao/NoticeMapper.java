9659c39cf637cf6844fd8105a40117ad4d726313
Heejun <gmlwns9863@gmail.com>
2025-10-10 22:26:57 +0900

    Feat. - 관리자 글쓰기 페이지 기능 추가 Update. - notice-adminWrite.html 타임리프 문으로 수정 - notice-adminWrite.js  html 코드 변경으로 필요없는 코드 제거

--- DashiMarket/src/main/java/edu/og/project/notice/model/dao/NoticeMapper.java
+++ DashiMarket/src/main/java/edu/og/project/notice/model/dao/NoticeMapper.java
@@ -10,11 +10,18 @@ import edu.og.project.notice.model.dto.Notice;
 @Mapper
 public interface NoticeMapper {
 
-	/**
+	/** 공지사항 목록 조회
 	 * @param boardType
 	 * @return 
 	 */
 	List<Notice> selectNoticeList(@Param("boardType") String boardType);
+	
+	
+	/** 공지사항 글 등록
+	 * @param notice
+	 * @return
+	 */
+	int insertNotice(Notice notice);
 
 
 
