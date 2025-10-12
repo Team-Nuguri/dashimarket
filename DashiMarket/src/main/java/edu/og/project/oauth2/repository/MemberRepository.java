429efc16cedab8973a5617cd019a89255385b276
Jihyeon <every3322@gmail.com>
2025-10-12 12:49:06 +0900

    커밋

---
+++ DashiMarket/src/main/java/edu/og/project/oauth2/repository/MemberRepository.java
@@ -0,0 +1,21 @@
+package edu.og.project.oauth2.repository;
+
+import edu.og.project.oauth2.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+// Member 엔티티의 DB 작업을 위한 Repository
+// 기본적인 CRUD 메서드를 제공하며, 추가적인 쿼리 메서드를 정의합니다.
+public interface MemberRepository extends JpaRepository<User, Long> {
+
+    /**
+     * 이메일을 이용하여 회원을 조회합니다.
+     * OAuth2 로그인에서 신규/기존 회원 여부를 판단할 때 사용됩니다.
+     * @param memberEmail 조회할 회원 이메일
+     * @return Optional<Member>
+     */
+    Optional<User> findByMemberEmail(String memberEmail);
+    
+}
