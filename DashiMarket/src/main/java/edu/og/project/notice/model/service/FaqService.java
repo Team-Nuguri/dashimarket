46a445fea8cc1b40d742604506e33c80966938d0
Heejun <gmlwns9863@gmail.com>
2025-10-14 00:33:45 +0900

    Feat. 자주 묻는 질문 기능 추가

--- DashiMarket/src/main/java/edu/og/project/notice/model/service/FaqService.java
+++ DashiMarket/src/main/java/edu/og/project/notice/model/service/FaqService.java
@@ -1,6 +1,22 @@
 package edu.og.project.notice.model.service;
 
+import java.util.List;
+import java.util.Map;
+
+import edu.og.project.common.dto.Image;
+import edu.og.project.notice.model.dto.Faq;
 
 public interface FaqService {
+    List<Faq> selectFaqList(Map<String, Object> paramMap);
+    int getFaqCount(String query);
+    Faq selectFaqDetail(int faqNo);
+    int insertFaq(Faq faq);
+    int insertImage(Image image);
+    int updateFaq(Faq faq);
+    int deleteImagesByBoardNo(String boardNo);
+    int deleteFaq(int faqNo);
+    
+    // 조회수 증가 메서드 추가
+    int increaseViewCount(int faqNo);
 
 }
--- DashiMarket/src/main/java/edu/og/project/notice/model/service/FaqServiceImpl.java
+++ DashiMarket/src/main/java/edu/og/project/notice/model/service/FaqServiceImpl.java
@@ -1,13 +1,71 @@
 package edu.og.project.notice.model.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import edu.og.project.common.dto.Image;
 import edu.og.project.notice.model.dao.FaqMapper;
+import edu.og.project.notice.model.dto.Faq;
 
 @Service
-public class FaqServiceImpl implements FaqService{
+public class FaqServiceImpl implements FaqService {
 	
 	@Autowired
 	private FaqMapper mapper;
+	
+	@Override
+	public List<Faq> selectFaqList(Map<String, Object> paramMap) {
+		return mapper.selectFaqList(paramMap);
+	}
+	
+	@Override
+	public int getFaqCount(String query) {
+		return mapper.getFaqCount(query);
+	}
+	
+	@Override
+	public Faq selectFaqDetail(int faqNo) {
+		return mapper.selectFaqDetail(faqNo);
+	}
+	
+	@Transactional
+	@Override
+	public int insertFaq(Faq faq) {
+		return mapper.insertFaq(faq);
+	}
+	
+	@Transactional
+	@Override
+	public int insertImage(Image image) {
+		return mapper.insertImage(image);
+	}
+	
+	@Transactional
+	@Override
+	public int updateFaq(Faq faq) {
+		return mapper.updateFaq(faq);
+	}
+	
+	@Transactional
+	@Override
+	public int deleteImagesByBoardNo(String boardNo) {
+		return mapper.deleteImagesByBoardNo(boardNo);
+	}
+	
+	@Transactional
+	@Override
+	public int deleteFaq(int faqNo) {
+		return mapper.deleteFaq(faqNo);
+	}
+	
+	@Transactional
+	@Override
+	public int increaseViewCount(int faqNo) {
+	    return mapper.increaseViewCount(faqNo);
+	}
+
 }
